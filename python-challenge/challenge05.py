5aef6bc69092d01bac8fb94e5415649df483f781
Mark Malabanan <mark.malabanan24@gmail.com>
2021-01-18 01:10:56 -0500

    Finished up to Challenge 6 of the Python Challenge

---
+++ python-challenge/challenge05.py
@@ -0,0 +1,23 @@
+import pickle
+import os
+
+dirPath = os.path.dirname(os.path.realpath(__file__))
+inputPath = "assets/banner.p"
+
+# Here, we're using pickle to unpickle (aka deserialize) a pickled (aka serialized) file
+# Pickling is kinda the same as serializing to JSON, but only in specific programming langs
+# like Python
+
+file = open(os.path.join(dirPath, inputPath), "rb")
+obj = pickle.load(file);
+file.close()
+
+# Once unpickled, the obj is an array of arrays, specifying the character to print to a line
+# and how many times to print it. It'll draw an ASCII art showing a word
+
+for i in obj:
+    line = ""
+    for j in i:
+        for k in range(0, j[1]):
+            line += j[0]
+    print(line)
---
+++ python-challenge/challenge06.py
@@ -0,0 +1,29 @@
+import os
+import zipfile
+import re
+
+dirPath = os.path.dirname(os.path.realpath(__file__))
+channelPath = "assets/channel.zip"
+file = zipfile.ZipFile(os.path.join(dirPath, channelPath))
+
+# Here, we're following the numbers again, like in challenge 4. The start is at readme.txt
+# Apparently, there are zip comments for each file, so we gotta collect those in order while
+# following the number trail. Printing it all out will show another ASCII art of a word.
+
+currentNumber = 90052
+comments = []
+while True:
+    path = os.path.join(dirPath, channelPath.format(currentNumber))
+    line = file.read("{0}.txt".format(currentNumber)).decode("utf-8")
+
+    # Reading the comments
+    comments.append(file.getinfo("{0}.txt".format(currentNumber)).comment.decode("utf-8"))
+    
+    print(line)
+    match = re.search("[0-9]+", line)
+    if match == None:
+        break
+
+    currentNumber = match.group()
+
+print("".join(comments))
